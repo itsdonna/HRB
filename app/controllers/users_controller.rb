46ac82ea37205c42bc12034cd383492319c42164
Donna Torres <todonnatorres@icloud.com>
2021-01-20 22:20:11 -0600

    added new, index, and show methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+
     def index
         @users = User.all
     end
@@ -6,4 +11,5 @@ class UsersController < ApplicationController
     def show
         @user = User.find(current_user)
     end
+    
 end
