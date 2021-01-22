0f41302dd5debef49d99ff3f83fa013797af6d0a
Donna Torres <todonnatorres@icloud.com>
2021-01-21 20:47:21 -0600

    add destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  
+
+  delete '/logout' => 'sessions#destroy'
+
   resources :tasks
   resources :users
   resources :projects
