92d808fa1307ab995ce09494325235734a576766
Donna Torres <todonnatorres@icloud.com>
2021-01-22 23:46:42 -0600

    can create and destroy with error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
         
     end
 
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:error] = "Email or password incorrect. Please try again."
+            redirect_to '/login'
+        end
+    end
+
     def destroy 
         session.delete(:user_id)
         redirect_to root_path
