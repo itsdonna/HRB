00eeb1c09512fab064151cf2eeadac0071366519
Donna Torres <todonnatorres@icloud.com>
2021-01-24 07:37:09 -0600

    added before action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,11 @@
 class ProjectsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @project = Project.new
+    end
+
+    def index
+        @projects = Project.all
+    end
 end
