f2844e2336769593e5155dd318ab02775aa27b36
Donna Torres <todonnatorres@icloud.com>
2021-01-24 14:12:34 -0600

    add show, index, and update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,40 @@
 class ProjectsController < ApplicationController
     before_action :redirect_if_not_logged_in
-    before_action :set_project, only:[:show, :edit, :update]
+    before_action :set_project, only: [:show, :edit, :update]
 
+    
     def new
         @project = Project.new
     end
     
     def create
-        @project = current_user.projects.build(project_params)
+        @project = Project.new(project_params)
         if @project.save
-            redirect_to projects_path
+            redirect_to project_path
         else
             render :new
         end
     end
+
+    def show
+        @project = Project.find(params[:id])
+    end
     
     def index
-        @projects = Project.order('name ASC').all
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @projects = @user.projects
+        else
+            @projects = Project.order_by_name
+        end
+        # @projects = Project.order('name ASC').all
+    end
+
+    def update
+        if @project.update(project_params)
+            redirect_to project_path
+        else
+            render :edit
+        end
     end
 
     private
