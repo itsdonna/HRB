e9a59750eaf8246da71f5386a7f6007700b4b285
Donna Torres <todonnatorres@icloud.com>
2021-01-24 00:38:23 -0600

    added private params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,22 @@
 class TasksController < ApplicationController
 
     def new
-       @tasks = Task.new 
+       @tasks = Task.new
+       @tasks.build_project
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to task_path(@task)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:item, :price, :task_id, :project_id, project_attributes:[:name])
     end
 end
