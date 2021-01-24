900322073dc943494a8fc1a4c79df08d795f863e
Donna Torres <todonnatorres@icloud.com>
2021-01-23 22:11:06 -0600

    added nested attribute

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validates :item, presence: true
+  validates :price, presence: true
+  
+  accepts_nested_attributes_for :project
 end
