c672681c404ca68defd9ec083e91e0523b9105b2
Donna Torres <todonnatorres@icloud.com>
2021-01-24 14:10:22 -0600

    add show and edit function

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,30 @@
 class ProjectsController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_project, only:[:show, :edit, :update]
 
     def new
         @project = Project.new
     end
-
+    
+    def create
+        @project = current_user.projects.build(project_params)
+        if @project.save
+            redirect_to projects_path
+        else
+            render :new
+        end
+    end
+    
     def index
-        @projects = Project.all
+        @projects = Project.order('name ASC').all
+    end
+
+    private
+    def set_project
+        @project = Project.find_by_id(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:name, :contractor, :description, :date)
     end
 end
