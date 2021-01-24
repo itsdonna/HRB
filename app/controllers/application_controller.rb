a1a6d030f78f9f3932794836fea25aca013e4bd4
Donna Torres <todonnatorres@icloud.com>
2021-01-24 07:36:35 -0600

    added helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in? #dont have to repeat logic in other views
+
+    private
+    def current_user
+      @current_user ||= User.find_by_id(session[:user])
+    end
+
+     def logged_in?
+       current_user != nil
+     end
+
+     def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+     end
 end
