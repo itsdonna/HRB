42cfc01d4e008ad067fcec9d44569d9368f16c58
Donna Torres <todonnatorres@icloud.com>
2021-01-24 14:13:10 -0600

    add show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def index
-        @users = User.all
-    end
-
     def create
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to @user
         else
             render :new
         end
