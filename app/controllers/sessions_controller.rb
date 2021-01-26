14e857c7edb814c6374c3832310b22589f066f29
Donna Torres <todonnatorres@icloud.com>
2021-01-26 07:36:59 -0600

    added omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,22 @@ class SessionsController < ApplicationController
 
     def destroy 
         session.delete(:user_id)
-        redirect_to root_path
+        redirect_to '/'
     end
 
+    def google
+        user = User.find_or_create_from_omniauth(auth)
+        if user.save
+          session[:user_id] = user.id
+          redirect_to user_path(user)
+        else
+          redirect_to root_path
+        end
+    end
+      
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
 end
