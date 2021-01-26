91c87b98ba7dde38d8b91a4ef3114178c5b943da
Donna Torres <todonnatorres@icloud.com>
2021-01-26 07:39:16 -0600

    added omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ApplicationRecord
     has_many :tasks
     has_many :projects, through: :tasks
 
-    has_secure_password
-
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
+    
+    has_secure_password
+
+    def self.find_or_create_from_omniauth(user_info)
+        User.first_or_create(uid: user_info[:uid]) do |user|
+            user.email = user_info["info"]["nickname"]
+            user.password = SecureRandom.hex
+        end
+    end
 end
