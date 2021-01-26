e213a94a570a34a4ba7b8d3dd7d0fe4d2e214178
Donna Torres <todonnatorres@icloud.com>
2021-01-26 07:37:43 -0600

    added validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ApplicationRecord
     has_many :tasks
     has_many :users, through: :tasks
     accepts_nested_attributes_for :tasks
+
+    validates :name, presence: true, uniqueness: true
+    validates :contractor, presence: true, uniqueness: true
+    validates :description, presence: true
+    validates :date, presence: true
+
+   
+
 end
