417c4de4cec028bbc83ce70dcf48b6c782eaa7a3
Donna Torres <todonnatorres@icloud.com>
2021-01-26 07:26:15 -0600

    updated private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in? #dont have to repeat logic in other views
+    helper_method :current_user, :logged_in? #dry by allowing acces to methods in views
 
     private
     def current_user
-      @current_user ||= User.find_by_id(session[:user])
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
-
-     def logged_in?
-       current_user != nil
-     end
-
-     def redirect_if_not_logged_in
-        redirect_to '/' if !logged_in?
-     end
+  
+    def logged_in?
+      !!session[:user_id]
+    end
+  
+    def redirect_if_not_logged_in
+      redirect_to '/' if !logged_in?
+    end
 end
